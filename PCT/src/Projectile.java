046db39370249d60547510d635bf2b67e9e2eeed
Julian Rengstorf <julian.rengstorf@nordakademie.de>
2016-10-14 15:57:24 +0100

    full functionality
    + fire() implemented
    + added notes to PDF

--- PCT/src/Projectile.java
+++ PCT/src/Projectile.java
@@ -8,6 +8,7 @@
 import java.text.DecimalFormat;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -24,10 +25,10 @@ public class Projectile extends Application {
 
 	public void init() {
 		// init method, setting default values
+
 		// Projectile Type - ComboBox
 		projectile_type_combobox.getItems().addAll("Adult Human", "Piano");
-		projectile_type_combobox.getSelectionModel().select(0); // default
-																// select
+		projectile_type_combobox.getSelectionModel().select(0); // default select
 
 		// initial speed - ToggleGroup
 		initial_speed_slow.setToggleGroup(initial_speed_toggleGroup);
@@ -35,18 +36,24 @@ public class Projectile extends Application {
 		initial_speed_fast.setToggleGroup(initial_speed_toggleGroup);
 		initial_speed_fast.setSelected(true);
 
+		// set up GridPane Layout
 		gp.addRow(0, projectile_type_label, projectile_type_combobox);
-		gp.addRow(1, mass_label, mass_textField);
+		gp.addRow(1, mass_label, mass_textField, mass_exception_label);
+		mass_exception_label.setId("massl");
+		GridPane.setConstraints(mass_exception_label, 2, 1, 4, 1);
 		gp.addRow(2, angle_label, angle_textField, angle_slider);
+		// set slider to span the 3 columns of RadioButtons below
 		GridPane.setConstraints(angle_slider, 2, 2, 3, 1);
-
+		gp.setHgap(5); // horizontal gap in pixels
+		gp.setVgap(5); // vertical gap in pixels
+		gp.setPadding(new Insets(5, 5, 5, 5)); // margins around the whole GridPane
 		gp.addRow(3, initial_speed_label, intitial_speed_textField, initial_speed_slow, initial_speed_medium,
 				initial_speed_fast);
 		gp.addRow(4, range_label, range_textField);
 		gp.addRow(5, height_label, height_textField);
 		gp.addRow(6, time_label, time_textField);
 		gp.addRow(7, fire_button, erase_button);
-		// fire_button.getStyleClass().add("fire");
+		fire_button.setId("fire");
 
 		// Initial Speed ToggleGroup
 		// use the .setUserData command of the radio button to store speeds
@@ -54,8 +61,7 @@ public class Projectile extends Application {
 		initial_speed_medium.setUserData("55");
 		initial_speed_fast.setUserData("100");
 
-		// Prevent the following TextFields from being editable: angle,initial
-		// speed, range, height, time
+		// Prevent the following TextFields from being editable: angle,initial speed, range, height, time
 		angle_textField.setEditable(false);
 		intitial_speed_textField.setEditable(false);
 		range_textField.setEditable(false);
@@ -67,31 +73,24 @@ public class Projectile extends Application {
 		// My version has 7 rows, you can look at the JavaFX API to see how to
 		// get controls to span more than one column
 
-		// Method call (not declaration!) to initialize the controls based on
-		// the projectile type.
+		// Method call to initialise the controls based on the projectile type
+		initalizeControlValues();
 
-		// Listener for angle Slider to set angle TextTield and the angle
-		// variable
+		// Listener for angle Slider to set angle TextTield and the angle variable
 		angle_slider.valueProperty().addListener((observable, oldValue, newValue) -> {
-			angle = (double) newValue;
-			angle_textField.setText(newValue.toString());
-			fire(); //update calculation with new angle
+			angle_textField.setText(df.format(newValue));
+			fire(); // update calculation with new angle
 		});
 
-		// prevent user from text input into mass field
 		mass_textField.textProperty().addListener((observable, oldValue, newValue) -> {
-			if (!newValue.matches("\\d*")) {
-				mass_textField.setText(newValue.replaceAll("[^\\d]", ""));
-			}
-			mass = Double.parseDouble(newValue);
-			fire(); //update calculation with new mass
+			fire(); // update calculation with new mass
 		});
 
 		// Listener for inital_speed ToggleGroup to set initital_speed TextField
 		initial_speed_toggleGroup.selectedToggleProperty().addListener((ov, toggle, new_toggle) -> {
 			intitial_speed_textField.setText((String) new_toggle.getUserData());
-//			initial_speed = Double.parseDouble(intitial_speed_textField.getText());
-			fire(); //update calculation with new initial speed
+			// initial_speed = Double.parseDouble(intitial_speed_textField.getText());
+			fire(); // update calculation with new initial speed
 		});
 
 		// Listener to call the fire() method when the fire button is pressed
@@ -99,13 +98,12 @@ public class Projectile extends Application {
 			fire();
 		});
 
-		// Listener to initialize control values if the projectile type is
-		// changed
+		// Listener to initialise control values if the projectile type is changed
 		projectile_type_combobox.setOnAction((event) -> {
 			initalizeControlValues();
 		});
 
-		// Listener to initialize control values if the erase button is pressed
+		// Listener to initialise control values if the erase button is pressed
 		erase_button.setOnAction((event) -> {
 			initalizeControlValues();
 		});
@@ -119,7 +117,6 @@ public class Projectile extends Application {
 		sc.getStylesheets().add("style.css");
 		primaryStage.setScene(sc);
 		primaryStage.show();
-		initalizeControlValues();
 	}
 
 	// Overridden stop method add functionality to this if you wish.
@@ -132,51 +129,69 @@ public class Projectile extends Application {
 		launch(args);
 	}
 
-	// Method to harvest values from controls, perform calculation and display
-	// the results
+	// Method to harvest values from controls, perform calculation and display the results
 	private void fire() {
-		// capture the values from the text fields outputting number errors
-		// where relevant
-				
+		try {
+			mass = Double.parseDouble(mass_textField.getText());
+			mass_exception_label.setText("");
+
+		} catch (NumberFormatException e) {
+			mass_exception_label.setText("Only numerical input allowed!");
+		}
+
+		initial_speed = Double.parseDouble(intitial_speed_textField.getText());
+		angle = Double.parseDouble(angle_textField.getText());
+		double angle_rad = Math.toRadians(angle);
+
+		// calculate the range of the projectile
+		range = initial_speed * initial_speed  * Math.sin(2 * angle_rad) 
+				/ gravitational_accelleration;
+
+		// calculate the max height of the projectile
+		height = initial_speed * initial_speed * Math.sin(angle_rad) * Math.sin(angle_rad)
+				/ (2 * gravitational_accelleration);
+
+		// calculate the flight time of the projectile
+		time = 2 * initial_speed * Math.sin(angle_rad)
+				/ gravitational_accelleration;
+
+		// capture the values from the text fields outputting number errors where relevant
+
 		// don't forget to convert your angle input to radians for use with
 		// Math.sin()
 
-		// calculate the range of the projectile
-
-		// calculate the flight time of the projectile
-
-		// calculate the max height of the projectile
-
 		// display the results in the relevant TextFields
+		range_textField.setText(df.format(range));
+		height_textField.setText(df.format(height));
+		time_textField.setText(df.format(time));
 	}
 
-	// Method to initalize the controls based on the selection of the projectile
-	// type
+	// Method to initalize the controls based on the selection of the projectile type
 	private void initalizeControlValues() {
 		// if the projectile type is Adult Human then
 		if (projectile_type_combobox.getValue() == "Adult Human") {
-			// inital the mass to 80kg
-			mass = 80;
-			mass_textField.setText(df.format(mass));
-					// Set slider scale 0 to 90, set slider value to 45 and ticks to 10
-			// units
+			// initialise the mass to 80kg
+			mass_textField.setText("80");
+			// Set angle slider scale 0 to 90, set slider value to 45 and ticks to 10 units
 			angle_slider.setMax(90);
 			angle_slider.setValue(45);
 			angle_slider.setMajorTickUnit(15);
-			// initalize the intital speed to fast
+			// set angle TextField to slider value
+			angle_textField.setText(df.format(angle_slider.getValue()));
+			// initialise the initial speed to fast
 			initial_speed_fast.setSelected(true);
 			intitial_speed_textField.setText((String) initial_speed_fast.getUserData());
 
 		} else {
-			// inital the mass to 400kg
-			mass = 400;
-			mass_textField.setText(df.format(mass));
-			// Set slider scale 0 to 40, set slider value to 20 and ticks to 10
-			// units
+			// initialise the mass to 400kg
+			mass_textField.setText("400");
+			// Set angle slider scale 0 to 40, set slider value to 20 and ticks to 10 units
 			angle_slider.setMax(40);
 			angle_slider.setValue(20);
 			angle_slider.setMajorTickUnit(10);
-			// initalize the intial speed to slow
+			// set angle TextField to slider value
+			angle_textField.setText(df.format(angle_slider.getValue()));
+			// initialise the initial speed to slow
 			initial_speed_slow.setSelected(true);
 			intitial_speed_textField.setText((String) initial_speed_slow.getUserData());
 		}
@@ -184,9 +199,8 @@ public class Projectile extends Application {
 		// display ticks etc
 		angle_slider.setShowTickMarks(true);
 		angle_slider.setShowTickLabels(true);
-		angle_slider.setSnapToTicks(true);
 		angle_slider.setBlockIncrement(5);
-		
+
 		// clear the results fields and variables
 		range_textField.clear();
 		height_textField.clear();
@@ -207,6 +221,7 @@ public class Projectile extends Application {
 	// Mass
 	private Label mass_label = new Label("Mass [kgs]");
 	private TextField mass_textField = new TextField();
+	private Label mass_exception_label = new Label();
 	private double mass = 0;
 
 	// Angle
@@ -216,7 +231,7 @@ public class Projectile extends Application {
 	private TextField angle_textField = new TextField();
 	private double angle = 0;
 	// Formating the values in the duration box
-	DecimalFormat df = new DecimalFormat("0");
+	DecimalFormat df = new DecimalFormat("#.##");
 
 	// Initial Speed
 	private Label initial_speed_label = new Label("Initial Speed [m/s]");
@@ -244,14 +259,16 @@ public class Projectile extends Application {
 
 	// Gravity
 	private static final double gravitational_accelleration = 9.81; // m/s/s
+	private static final double gravitational_accelleration_mars = 3.711; // m/s/s
+	private static final double gravitational_accelleration_moon = 1.622; // m/s/s
 
 	// Calculate
 	private Button fire_button = new Button("Fire!");
-	private Button erase_button = new Button("Reset");
+	private Button erase_button = new Button("Erase");
 }
 
 // don't break away form the template
 // change slider according to value
 
 // inTitial_speed_textField!! can we change this?
-//decimalformat? use for mass or for time?
+// decimalformat? use for mass or for time?
